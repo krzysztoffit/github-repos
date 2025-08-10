1072d363669a0baa1b42f5a5c18d8624630678cb
krzysztoffit <krzysztof.fit@hotmail.com>
2025-08-10 21:12:46 +0200

    Initial commit

---
+++ src/main/java/com/github_repos/GithubReposApplication.java
@@ -0,0 +1,13 @@
+package com.github_repos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubReposApplication.class, args);
+	}
+
+}
